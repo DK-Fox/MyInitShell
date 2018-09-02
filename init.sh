e6ec927be8e824236bee46e581da5406d260d2b6
DK-Fox <syc0441@163.com>
2018-09-02 11:44:39 +0800

    v1

--- init.sh
+++ init.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
-#install  zsh  oh-my-zsh
+
+cp -f sources.list /etc/apt/
+apt-get update
+apt-get upgrade
+
+apt-get install tmux
 apt-get install zsh
+apt-get install fcitx-googlepinyin
+apt-get install libreoffice
+apt-get install pandoc
+apt-get install ctags
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - |sh
-#~/.zshrc theme "ys"  ~/.bashrc zsh
-#install  spf13
-mkdir ~/Downloads/spf13
-cd ~/Downloads/spf13
+
+useradd -m dkfox
+usermod -a -G sudo dkfox
+chsh -s /usr/bin/zsh dkfox
+chsh -s /usr/bin/zsh
+
+cd ~
+su dkfox
+
+cp  -r init ~
+cd ~/init
+
+cp .zshrc .tmux.conf ~
+
+cd ~/Downloads
 git clone https://github.com/spf13/spf13-vim
 cd spf13-vim
 ./bootstrap.sh
-#install  tmux
-apt-get install tmux
+
+cd ~
+cp -f .vimrc .vimrc.bundles ~/.spf13-vim-3/
+
+#记得切换到root删除init，并给dkfox加密码
+#下载npm，执行sudo npm -g instant-markdown-d
+#下载chrome
+#vim执行:PluginInstall
+#firefox里about:config,把dom.allow_scripts_to_windows设置为true
+#下载虚拟机
